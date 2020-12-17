933c1ba4d65bb24532c4bd4800e828bfa8c463d4
Remmington Pascone <remmingtonpearce@gmail.com>
2020-12-17 15:42:48 -0800

    added confirm prompts for desired characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Array of special characters to be included in password
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+
+//Generating password 
+function generatePassword (){
+  var options = getPasswordOptions();
+
+}
+
+
+function getPasswordOptions(){
+  var length = parseInt(prompt("Enter desired length of password"))
+  //Conditional statement to ensure user input 
+  //Check password length
+
+  var hasSpecialChar = confirm("Click ok to confirm allowing special characters in your password")
+
+  var hasNumbers = confirm("Click ok to confirm allowing numbers in your password")
+
+
 
 }
 
